827a88a705ecdb27acd7453daa02360dcfeecbd8
Rohit Gujarathi <gujju.rohit@gmail.com>
2020-04-10 19:38:06 +0530

    split the jumping logic and function call into 2 APIs

--- lib/syscall_asm.s
+++ lib/syscall_asm.s
@@ -8,15 +8,22 @@
 jumptable:
 	.word set_led_2
 
+.section .text.jump2func
+.globl jump2func
+.type jump2func,%function
+jump2func:
+    ldr r2, =jumptable
+    ldr ip, [r2, r0]
+    pop {r0,r1,r2,r3}
+    bx  ip
+
 .section .text.set_led
 .globl set_led
 .type  set_led,%function
 set_led:
-    push {r5}
-    ldr r5, =jumptable
-    ldr ip, [r5, #0]
-    pop {r5}
-    bx  ip
+    push {r0,r1,r2,r3}
+    mov  r0, #0
+    b    jump2func
 .size  set_led, .-set_led
 
 
