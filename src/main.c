db9bbef9214bb49ddbb7680b9253cd4fd4341099
Rohit Gujarathi <gujju.rohit@gmail.com>
2020-06-09 08:58:16 +0530

    tried to do dynamic allocation of thread stack

--- src/main.c
+++ src/main.c
@@ -47,18 +47,72 @@ void blinky() {
     }
 }
 struct k_thread blinky_thread;
-k_thread_stack_t blinky_stack[4096];
+
+//uint8_t __aligned(8) blinky_buffer[4096];
+//k_thread_stack_t* blinky_stack = &blinky_buffer[2048];
+
+//uint8_t __aligned(8) blinky_stack[4096];
+
+//k_thread_stack_t __aligned(8) blinky_stack[4096]; //does not work
+
+
+// The following works because of the keyword z_thread_stack_element 
+// scripts/gen_kobject_list.py parses the generated elf file for that keyword
+// and adds it to the generated gperf hash  
+
+//struct z_thread_stack_element __aligned(8) blinky_stack[4096];
+
+//struct z_thread_stack_element __attribute__((__aligned__(4096))) blinky_stack[4096];
+
+//K_THREAD_STACK_DEFINE(blinky_stack, 4096);
+
+//struct z_thread_stack_element __attribute__((__aligned__(((1 << (31 - __builtin_clz(4096))) < 4096 ? 1 << (31 - __builtin_clz(4096) + 1) : 1 << (31 - __builtin_clz(4096)))))) blinky_stack[((1 << (31 - __builtin_clz(4096))) < 4096 ? 1 << (31 - __builtin_clz(4096) + 1) : 1 << (31 - __builtin_clz(4096)))];
 
 int main ( void ) {
-    SetLed(LED0, LED_ON);
+	k_thread_system_pool_assign(k_current_get());	
+	// Does not work
+	//struct z_thread_stack_element *blinky_stack = k_malloc(4096);
+	//blinky_stack = Z_STACK_PTR_ALIGN(blinky_stack);
+
+//	struct k_thread* blinky_thread =  k_object_alloc(K_OBJ_THREAD);
+//	if(blinky_thread == NULL){
+//		printk("Could not allocate blinky_thread");
+//		return -1;
+//	}
+
+	printk("size of struct z_stack_data: %d\n",sizeof(struct z_stack_data));	
+
+	struct z_thread_stack_element* blinky_stack = k_object_alloc(K_OBJ_THREAD_STACK_ELEMENT,4096);
+	
+	if(blinky_stack == NULL){
+		printk("Could not allocate blinky_stack");
+		return -1;
+	}
+
+
+	
+
+	//printk("Aligned to %d\n",((1 << (31 - __builtin_clz(4096))) < 4096 ? 1 << (31 - __builtin_clz(4096) + 1) : 1 << (31 - __builtin_clz(4096))));
+	//printk("Blinky stack size %d\n",((1 << (31 - __builtin_clz(4096))) < 4096 ? 1 << (31 - __builtin_clz(4096) + 1) : 1 << (31 - __builtin_clz(4096)))); 
+    
+	//printk("Alignment of stack %d\n",__alignof__(blinky_stack));
+	//printk("Size of stack %d\n",sizeof(blinky_stack));
+
+	//for(uint32_t i = 0; i < sizeof(blinky_stack); i++){
+	//	blinky_stack[i] = i;
+	//}
+
+	SetLed(LED0, LED_ON);
     k_thread_create(&blinky_thread,
                     blinky_stack,
-                    sizeof(blinky_stack),
+                    4096,
                     blinky,
                     NULL, NULL, NULL,
                     3, K_USER, K_NO_WAIT);
-    //while (1) {
-    //}
+
+	while (1) {
+    	k_msleep(1000);
+	}
     return 0;
 }
 
